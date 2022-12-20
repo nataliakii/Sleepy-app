4d6c9ab08f5dc491d32809ef1da8777b4d0a7eab
Natalia Kirejeva <nataliaki@icloud.com>
2022-12-20 22:32:21 +0200

    new commit for deploy which is not working and i give up#3

--- config/prod.js
+++ config/prod.js
@@ -1,8 +1,4 @@
-// module.exports = {
-//   MONGODB_URI: process.env.MONGODB_URI,
-//   TOKEN_SECRET: process.env.TOKEN_SECRET,
-// };
 module.exports = {
-  MONGODB_URI: 'mongodb://localhost/sleepyApp',
-  TOKEN_SECRET: 'helloworld'
+  MONGODB_URI: process.env.MONGODB_URI,
+  TOKEN_SECRET: process.env.TOKEN_SECRET,
 };
