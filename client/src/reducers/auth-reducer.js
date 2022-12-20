67d2275d460674927c318b4635a48ace43331c6c
Natalia Kirejeva <nataliaki@icloud.com>
2022-12-20 23:06:51 +0200

    new commit for deploy which is not working and i give up#5 and couple of new logs in auth-reducer #2

--- client/src/reducers/auth-reducer.js
+++ client/src/reducers/auth-reducer.js
@@ -12,19 +12,19 @@ const INITIAL_STATE = {
 export default function AuthReducer(state = INITIAL_STATE, action) {
   switch (action.type) {
     case 'AUTH_USER':
-      console.log('this comes from AUTH REDUCER');
+      console.log('this comes from AUTH REDUCER', action.payload);
       return {
         authenticated: action.payload.token,
         name: action.payload.name,
-        email: action.payload.email,
         nameKid: action.payload.nameKid,
         kidBD: new Date(action.payload.kidBD).toLocaleDateString(),
+        email: action.payload.email,
       };
     case 'LOG_OUT':
-      console.log('this comes from LOG OUT');
+      console.log('this comes from LOG OUT', action.payload);
       return INITIAL_STATE;
     case 'AUTH_ERROR':
-      console.log('this comes from AUTH ERROR');
+      console.log('this comes from AUTH ERROR,action.payload');
       return { ...state, errorMessage: action.payload };
     default:
       return state;
