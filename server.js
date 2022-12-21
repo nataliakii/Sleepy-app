cf3e68248da53a88c20acd149cfaabb661a205c6
Natalia Kirejeva <nataliaki@icloud.com>
2022-12-21 17:34:02 +0200

    new commit with new logs #2

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(cors());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
   const path = require("path");
-  app.get("*", (req, res) => {
+  app.get("/*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
