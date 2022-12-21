8fb91d9ade566f57e4dc0c140c6da3d8dde02069
Natalia Kirejeva <nataliaki@icloud.com>
2022-12-21 18:07:19 +0200

    new commit with new logs #6

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -202,7 +202,6 @@ export const fetchLocation = () => (dispatch) => {
 //     const artworkData = transformData(request);
 //     artworkData.imageURL = `https://www.artic.edu/iiif/2/${artworkData.imageId}/full/843,/0/default.jpg`;
 
-
 //       type: 'FETCH_ARTWORK',
 //       payload: artworkData,
 
