986f1dead2b2d3c03366010288cecc7c8efb6f9e
Natalia Kirieieva <105832866+nataliakii@users.noreply.github.com>
2022-12-21 16:28:23 +0200

    Update server.js

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ if (process.env.NODE_ENV === "production") {
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
+}
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
